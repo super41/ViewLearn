16f1d940dfba17405b71525748808cc678e617ef
super41 <279870636@qq.com>
2017-09-24 21:48:44 +0800

    first commit

---
+++ app/src/main/java/activity/right/com/viewlearn/View/BaseView.java
@@ -0,0 +1,18 @@
+package activity.right.com.viewlearn.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Xjp on 2017-09-24.
+ */
+public class BaseView  extends View {
+    public BaseView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseView(Context context) {
+        super(context);
+    }
+}
